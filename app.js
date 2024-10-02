f9541b8e3233c9c792a29dca9f33c4528483d723
Adarsh Singh <127168959+AdarshSingh9@users.noreply.github.com>
2024-10-02 18:36:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetId = event.target.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
+
+const toggleButton = document.getElementById('darkModeToggle');
+    toggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
